18f7cde8c6d6764ad938ae0db5a4dd0a7b00af79
Konstantin Tenman <ktenman@users.noreply.github.com>
2018-04-29 23:48:53 +0300

    Fix

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@
 cd /root/flashcards/
 docker-compose -f src/main/docker/app.yml down
 git pull
-./mvnw verify -Pprod dockerfile:build &
-/usr/local/bin/docker-compose -f /root/flashcards/src/main/docker/app.yml up -d &
+./mvnw verify -Pprod dockerfile:build
+/usr/local/bin/docker-compose -f /root/flashcards/src/main/docker/app.yml up -d
 docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
