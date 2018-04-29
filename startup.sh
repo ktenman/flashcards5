786e37e26f33bba44151bf7957044de4cb2f6f11
Konstantin Tenman <ktenman@users.noreply.github.com>
2018-04-29 23:23:26 +0300

    Change actions order

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd /root/flashcards/
 docker-compose -f src/main/docker/app.yml down
-docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
 git pull
 ./mvnw verify -Pprod dockerfile:build &
 docker-compose -f src/main/docker/app.yml up -d &
+docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
 
