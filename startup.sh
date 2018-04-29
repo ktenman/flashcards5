744697eed68149e2746a20a3a934ec7fa77d2510
Konstantin Tenman <ktenman@users.noreply.github.com>
2018-04-30 00:01:48 +0300

    Improve

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
 cd /root/flashcards/
 docker-compose -f src/main/docker/app.yml down
 git pull
 ./mvnw verify -Pprod dockerfile:build
-/usr/local/bin/docker-compose -f /root/flashcards/src/main/docker/app.yml up -d
 docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
+/usr/local/bin/docker-compose -f /root/flashcards/src/main/docker/app.yml up -d
