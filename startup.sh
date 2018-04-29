5ebba214778ddaace0e70d483d817bd7ef7fb20b
Konstantin Tenman <ktenman@users.noreply.github.com>
2018-04-29 23:14:25 +0300

    Refactor

--- startup.sh
+++ startup.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 cd /root/flashcards/
 docker-compose -f src/main/docker/app.yml down
 docker rmi $(docker images -a|grep "<none>"|awk '$1=="<none>" {print $3}')
 git pull
-./mvnw verify -Pprod dockerfile:build
-docker-compose -f src/main/docker/app.yml up -d
+./mvnw verify -Pprod dockerfile:build &
+docker-compose -f src/main/docker/app.yml up -d &
+
