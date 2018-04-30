769c2c6af85b4617465304c816532d437675fa24
Konstantin Tenman <ktenman@users.noreply.github.com>
2018-04-30 11:03:11 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,6 +30,5 @@ cd
 git clone https://github.com/ktenman/flashcards5.git flashcards
 cd flashcards
 sudo chmod a+x mvnw
-./mvnw verify -Pprod dockerfile:build
-docker-compose -f src/main/docker/app.yml up -d
+#./mvnw verify -Pprod dockerfile:build && docker-compose -f src/main/docker/app.yml up -d
 
